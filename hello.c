c5f2de0606e34f2164b7459d275f8f7ffd01c222
Jared <janderson164@huskers.unl.edu>
2021-08-27 01:41:55 +0000

    added my major to the output

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Jared Anderson
+ *         cbourke@cse.unl.edu
+ * Date: 2021/08/24
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Jared Anderson!\n");
+  printf("Computer Science\n");
+
+  return 0;
+}
